01f25c01e8851e6dd7f1f12e317597ccaa428218
FoxedGuy <gradziuk.damian@o2.pl>
2025-03-02 18:10:25 +0100

    The most simple and stupid circle detection

---
+++ hough_circle_cpu.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+
+std::pair<int,int> hough_circles(unsigned char* data, int height, int width, int radius){
+    
+    // accum 
+    unsigned int *acc = new unsigned int[height*width];
+    memset(acc, 0, height*width*sizeof(unsigned int));
+
+    // fill accum
+    for(int y = 0; y < height; y++){
+        for (int x = 0; x < width; x++){
+            if (data[y*width + x] > 0){
+                // draw circle in accum
+                for (int i = 0; i < 360; i++){
+                    int a = (float)x - radius * cos(i);
+                    int b = (float)y - radius * sin(i);
+                    if (a >= 0 && a < width && b >= 0 && b < height){
+                        acc[b*width + a]++;
+                    }
+                }
+            }
+        }
+    }
+
+    //get max value in accum
+    int max = 0;
+    int max_y = 0;
+    int max_x = 0;
+    for (int y = 0; y < height; y++){
+        for (int x = 0; x < width; x++){
+            if (acc[y*width + x] > max){
+                max = acc[y*width + x];
+                max_y = y;
+                max_x = x;
+            }
+        }
+    }
+
+    return std::pair<int,int>(max_x, max_y);
+}
+
+int main(int argc, char** argv){
+    std::string path = "../pictures_circles/";
+    
+    std::string img_name = argv[1];
+    std::string img_path = path + img_name;
+    cv::Mat img = cv::imread(img_path, 1);
+    
+    if (!img.data){
+        printf("No image data \n");
+        return -1;
+    }
+
+    cv::Mat blur;
+    cv::Mat edges;
+    cv::GaussianBlur(img, blur, cv::Size(9,9), 2, 2);
+    cv::Canny(blur, edges, 100, 200);
+
+    std::pair<int,int> center = hough_circles(edges.data, edges.rows, edges.cols, 100);
+
+    cv::circle(img, cv::Point(center.first, center.second), 100, cv::Scalar(0, 0, 255), 2);
+
+    cv::imshow("Hough Circle", img);
+    cv::waitKey(0);
+
+    return 0;
+}
