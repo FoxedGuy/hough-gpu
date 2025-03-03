931561e0e861b75babec7873c7e93c1095760747
FoxedGuy <gradziuk.damian@o2.pl>
2025-03-03 12:05:33 +0100

    radii range

--- hough_circle_cpu.cpp
+++ hough_circle_cpu.cpp
@@ -1,33 +1,37 @@
 #include<iostream>
 #include<opencv2/opencv.hpp>
 
-std::vector<std::pair<int,int>> hough_circles(unsigned char* data, int height, int width, int radius, unsigned int threshold){
+std::vector<std::tuple<int,int,int>> hough_circles(unsigned char* data, int height, int width, int radius_min, int radius_max, unsigned int threshold){
     
     // accum 
-    unsigned int *acc = new unsigned int[height*width];
-    memset(acc, 0, height*width*sizeof(unsigned int));
+    unsigned int *acc = new unsigned int[height*width*(radius_max-radius_min+1)];
+    memset(acc, 0, height*width*(radius_max-radius_min+1)*sizeof(unsigned int));
 
     // fill accum
     for(int y = 0; y < height; y++){
         for (int x = 0; x < width; x++){
             if (data[y*width + x] > 0){
                 // draw circles in accum
-                for (int i = 0; i < 360; i++){
-                    int a = (float)x - radius * cos(i);
-                    int b = (float)y - radius * sin(i);
-                    if (a >= 0 && a < width && b >= 0 && b < height){
-                        acc[b*width + a]++;
+                for (int r = radius_min; r <= radius_max; r++){
+                    for (int i = 0; i < 360; i++){
+                        int a = (float)x - r * cos(i);
+                        int b = (float)y - r * sin(i);
+                        if (a >= 0 && a < width && b >= 0 && b < height){
+                            acc[(r - radius_min)*height*width + b*width + a]++;
+                        }
                     }
                 }
             }
         }
     }
 
-    std::vector<std::pair<int,int>> centers;
-    for (int y = 0; y < height; y++){
-        for (int x = 0; x < width; x++){
-            if (acc[y*width + x] > threshold){
-                centers.push_back(std::pair<int,int>(x,y));
+    std::vector<std::tuple<int,int,int>> centers;
+    for (int r = radius_min; r <= radius_max; r++){
+        for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
+                if (acc[(r - radius_min)*height*width + y*width + x] > threshold){
+                    centers.push_back(std::make_tuple(x,y,r));
+                }
             }
         }
     }
@@ -39,7 +43,8 @@ int main(int argc, char** argv){
     std::string path = "../pictures_circles/";
     
     std::string img_name = argv[1];
-    int radius = std::stoi(argv[2]);
+    int radius_min = std::stoi(argv[2]);
+    int radius_max = std::stoi(argv[3]);
     std::string img_path = path + img_name;
     cv::Mat img = cv::imread(img_path, 1);
     
@@ -53,10 +58,10 @@ int main(int argc, char** argv){
     cv::GaussianBlur(img, blur, cv::Size(9,9), 2, 2);
     cv::Canny(blur, edges, 100, 200);
 
-    std::vector<std::pair<int,int>> centers = hough_circles(edges.data, edges.rows, edges.cols, radius,200);
+    std::vector<std::tuple<int,int,int>> centers = hough_circles(edges.data, edges.rows, edges.cols, radius_min, radius_max,200);
 
     for (auto center: centers){
-        cv::circle(img, cv::Point(center.first, center.second), radius, cv::Scalar(255, 0, 255), 2);
+        cv::circle(img, cv::Point(std::get<0>(center), std::get<1>(center)), std::get<2>(center), cv::Scalar(255, 0, 255), 2);
     }
 
     cv::imshow("Hough Circle", img);
